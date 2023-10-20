e1a1d9d67d551c175a3c342d13ffc2dfc54a00a3
Emmanuel <emmanuel@gmail.com>
2023-10-20 13:03:36 -0300

    ehe

---
+++ src/app/ehemunicipal/page.jsx
@@ -0,0 +1,18 @@
+import { NavMain } from "../../components/NavMain";
+import { Footer } from "../../components/Footer";
+
+export default function Agricultura() {
+    return (
+        <>
+            <NavMain />
+
+            <p>
+
+                EHE MUNICIPAL
+            </p>
+        
+            <Footer />
+        
+        </>
+    );
+}
